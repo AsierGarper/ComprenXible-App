47d71d57c1d8c21f1cc7acce8506a6543a6d61ff
EnriqueTheDog <84322686+EnriqueTheDog@users.noreply.github.com>
2021-07-08 17:48:46 +0200

    Authorization v0.1

---
+++ comprenXible_API/DTO/UserCredentials.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace comprenXible_API.DTO
+{
+    public class UserCredentials
+    {
+            public string UserEmail { get; set; }
+            public string Password { get; set; }
+        
+    }
+}

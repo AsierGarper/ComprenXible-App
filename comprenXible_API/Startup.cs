df103092d4528795484587d03201284d7d7802b6
EnriqueTheDog <84322686+EnriqueTheDog@users.noreply.github.com>
2021-07-10 16:33:59 +0200

    solved issue with swagger

--- comprenXible_API/Startup.cs
+++ comprenXible_API/Startup.cs
@@ -81,8 +81,8 @@ namespace comprenXible_API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "comprenXible_API v1"));
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "comprenXible_API v1"));
             }
 
             app.UseHttpsRedirection();

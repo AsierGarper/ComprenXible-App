438aba52b6d6b4aa96190279c53b93fcab3457d3
Asier Garper <asiergarper@gmail.com>
2021-07-15 10:45:07 +0200

    Corregido el seteo de resultados en Area Personal tras collapso de git, redireccionamiento de Logout a Home para evitar chasheo

--- comprenXible_React/src/pages/PersonalArea/PersonalArea.js
+++ comprenXible_React/src/pages/PersonalArea/PersonalArea.js
@@ -7,10 +7,8 @@ import axios from "axios";
 function PersonalArea(props) {
 
     let sessionUserCredentials = sessionStorage.getItem("sessionUserCredentials");
-    let convertUser = JSON.parse(sessionUserCredentials);
-
-    console.log(sessionUserCredentials.name);
-    console.log(sessionUserCredentials.email);
+    sessionUserCredentials = JSON.parse(sessionUserCredentials);
+    var tests = sessionUserCredentials.tests;
 
     const [name, setName] = useState(sessionUserCredentials.name)
     const [email, setEmail] = useState(sessionUserCredentials.email)
@@ -22,7 +20,6 @@ function PersonalArea(props) {
     function changeEmail(event) {
         setEmail(event.target.value);
     }
-
 
     function saveChanges() {
         let changedUser = { "name": name, "email": sessionUserCredentials.email, "newEmail": email, "password": sessionUserCredentials.password };
@@ -37,64 +34,64 @@ function PersonalArea(props) {
             .catch(function (error) {
                 console.log(error);
             })
-        
 
-        }
+    }
 
-        let tests = convertUser.tests;
+    return (<>
+        <div className="PersonalArea">
+            <Navbar sessionUserCredentials={props.sessionUserCredentials} setSessionUserCredentials={props.setSessionUserCredentials} />
+            <div className="wrapper wrapper-filled">
+                <div className="textContainer">
+                    <h4>Modifica tus datos:</h4>
+                    <hr></hr>
+                    <form action="modifyUserData" className="userDataForm" onSubmit={(e) => e.preventDefault()}>
+                        <label htmlFor="userName">Nombre:</label><br></br>
+                        <input type="text" id="userName" name="userForm" value={name} onChange={event => changeName(event)} /><br></br>
+                        <label htmlFor="userEmail">Email:</label><br></br>
+                        <input type="mail" id="userEmail" name="userForm" value={email} onChange={event => changeEmail(event)} /><br></br>
+                        <button type="submit" className="button button--bgTransparent-white" onClick={saveChanges}>Guardar cambios</button>
+                    </form>
+                    <br></br>
+                    <h4>Análisis realizados:</h4>
+                    <hr></hr>
+                    <table id="testRecords">
+                        <tr>
+                            <th>Fecha</th>
+                            <th>Resultados</th>
+                        </tr>
 
-        return (<>
-            <div className="PersonalArea">
-                <Navbar sessionUserCredentials={props.sessionUserCredentials} setSessionUserCredentials={props.setSessionUserCredentials} />
-                <div className="wrapper wrapper-filled">
-                    <div className="textContainer">
-                        <h4>Modifica tus datos:</h4>
-                        <hr></hr>
+                        {(sessionUserCredentials.tests).length !== 0 && sessionUserCredentials != null ?
+                            tests.map((test, index) => {
+                                let dateParse = new Date(test.date)
+                                let date = `${dateParse.getDate()}-${dateParse.getMonth() + 1}-${dateParse.getFullYear()}`
+                                return (
 
-                        <form action="modifyUserData" className="userDataForm" onSubmit={(e) => e.preventDefault()}>
-                            <label htmlFor="userName">Nombre:</label><br></br>
-                            <input type="text" id="userName" name="userForm" value={name} onChange={event => changeName(event)} /><br></br>
-                            <label htmlFor="userEmail">Email:</label><br></br>
-                            <input type="mail" id="userEmail" name="userForm" value={email} onChange={event => changeEmail(event)} /><br></br>
-                            <button type="submit" className="button button--bgTransparent-white" onClick={saveChanges}>Guardar cambios</button>
-                        </form>
-
-                        <br></br>
-                        <h4>Análisis realizados:</h4>
-                        <hr></hr>
-                        <table id="testRecords">
-
+                                    <tr key={index}>
+                                        <td>{date}</td>
+                                        <td>{test.score}</td>
+                                    </tr>
+                                )
+                            })
+                            :
                             <tr>
-                                <th>Fecha</th>
-                                <th>Resultados</th>
+                                <td>-</td>
+                                <td>No existen resultados.</td>
                             </tr>
-                            {
-                                tests.map((test, index) => {
-                                    let dateParse = new Date(test.date)
-                                    let date = `${dateParse.getDate()}-${dateParse.getMonth() + 1}-${dateParse.getFullYear()}`
-                                    return (
-
-                                        <tr key={index}>
-                                            <td>{date}</td>
-                                            <td>{test.score}</td>
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </table>
-                        {/* <iframe title="map"
+                        }
+                    </table>
+                    {/* <iframe title="map"
                         width="600"
                         height="450"
                         loading="lazy"
                         allowfullscreen
                         src="https://www.google.com/maps/embed/v1/place?key=AIzaSyD9ecVzHiRU3frlwA9-R-q40p5GlVEXilU&q=current+location">
                     </iframe> */}
-                    </div>
-
                 </div>
-                <Footer />
+
             </div>
-        </>)
-    }
+            <Footer />
+        </div>
+    </>)
+}
 
 export default PersonalArea;
--- comprenXible_React/src/pages/home/Home.js
+++ comprenXible_React/src/pages/home/Home.js
@@ -6,8 +6,8 @@ import './Home.css';
 import ProfilePicture1 from '../images/mireia.png';
 import ProfilePicture2 from '../images/asier.png';
 import ProfilePicture3 from '../images/enrique.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimesCircle } from '@fortawesome/free-regular-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faTimesCircle } from '@fortawesome/free-regular-svg-icons'
 import Navbar from '../../components/navbar/Navbar';
 import Footer from '../../components/footer/Footer.js';
 
@@ -41,16 +41,16 @@ function Home(props) {
                     <Link onClick={tried} to={route} className="button button--bgTransparent-white" >COMENZAR TEST</Link>
                     {tryTest ? <p className="incorrectUser">Debes iniciar sesion para realizar el test.</p> : ""}
                 </div>
-                </div>
+            </div>
             <div className="wrapper wrapper-filled">
                 <div className="textSection">
                     <h4>¿En que consiste?</h4>
                     <hr></hr>
                     <p>ComprenXible App es un sistema de detección de sintomas depresivos, cuadros de ansiedad y demás afecciones derivadas del aislamiento.
                         Durante la pandemia, la población general ha tenido menos contacto con sus amigos y conocidos y se ha incrementado el sentimiento de apatía. El aislamiento ha generado soledad y angustia, y se han incrementado los trastornos alimenticios.
-                    </p> 
-                    <p> Sobre todo para los más jóvenes, la imposibilidad de estar con sus amigos y desarrollarse en un entorno social normal ha sido devastadora. La incomunicación, la falta de intimidad y el hecho de no poder separarse de la familia han sido factores importantes en este proceso. </p>                        
-                    <p> Comprenxible utiliza la analítica predictiva para detectar posibles cuadros sintomáticos de ansiedad y depresión. Tras hacer nuestro test en dos pasos, los usuarios reciben al instante los análisis y, en caso de ser positivos, los pasos recomendados a seguir.</p>                    
+                    </p>
+                    <p> Sobre todo para los más jóvenes, la imposibilidad de estar con sus amigos y desarrollarse en un entorno social normal ha sido devastadora. La incomunicación, la falta de intimidad y el hecho de no poder separarse de la familia han sido factores importantes en este proceso. </p>
+                    <p> Comprenxible utiliza la analítica predictiva para detectar posibles cuadros sintomáticos de ansiedad y depresión. Tras hacer nuestro test en dos pasos, los usuarios reciben al instante los análisis y, en caso de ser positivos, los pasos recomendados a seguir.</p>
                 </div>
                 <div className="textSection">
                     <h4>¿Quienes somos?</h4>
@@ -61,8 +61,8 @@ function Home(props) {
                         <div className="infoContainer">
                             <img src={ProfilePicture1} alt="" className="profilePicture"></img>
                             <p>Mireia Taboada Zapatero</p>
-                            <p>taboadamirella@gmail.com</p>                            
-                            <p>PENDIENTE PONER EMOTICONOOOOOOOOOOOS LINKEDIN Y GITHUB</p>
+                            <p>taboadamirella@gmail.com</p>
+                            {/* <p>PENDIENTE PONER EMOTICONOOOOOOOOOOOS LINKEDIN Y GITHUB</p> */}
                             <hr></hr>
                         </div>
                         <div className="infoContainer">
